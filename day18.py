1955aaa031882bf8b120859bcea2817bc83a8464
jcraitz <jcraitz@ncsu.edu>
2021-01-08 15:21:50 -0500

    working!

--- day18.py
+++ day18.py
@@ -38,7 +38,8 @@ def read_data(fn):
 
 def add_numbers(line):
     numb = 0
-    matches = re.finditer(r"\d*[\+\*]\d*", line, re.MULTILINE)
+    clean_match = line.replace('(', '').replace(')', '').replace(' ', '')
+    matches = re.finditer(r"\d*[\+\*]\d*", clean_match, re.MULTILINE)
     for matchNum, match in enumerate(matches, start=1):
         if matchNum == 1:
             numb = eval(match.group())
@@ -49,19 +50,22 @@ def add_numbers(line):
 def regex_match(line):
     regex = r"\([\d\s\+\*]+\)"
     r1 = re.findall(regex, line)
-    print(line)
+    # 5 * 9 * (7 * 3 * 3 + 9 * 3 + 56)
     for match in r1:
-        clean_match = match.replace('(', '').replace(')', '').replace(' ', '')
-        numb = add_numbers(clean_match)
-        print(line.replace(match, numb))
-        print(numb)
-        
+      numb = add_numbers(match)
+      line = line.replace(match, str(numb))
+    return line
 
 def main():
-    data = read_data("test.txt")
-    # data = read_data("input.txt")
+    # data = read_data("test.txt")
+    data = read_data("input.txt")
+    total = 0
     for line in data:
-        regex_match(line)
+        line = regex_match(line)
+        while '(' in line:
+            line = regex_match(line)
+        total += int(add_numbers(line))
+    print(total)
 
     #print(data[:5])
 
